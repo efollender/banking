8f836b74b70316e636073456befcca5a4056578b
Emma Follender <efollender@gmail.com>
2014-06-13 18:26:13 -0400

    changed  banking

--- banking.rb
+++ banking.rb
@@ -1,10 +1,11 @@
 class Person
-	def initialize(name, cash)
+	def initialize(name,cash)
 		@name = name
 	end
 end
 class Bank
 	def initialize(name,place)
 		@name = name
+		@place = place
 	end	
 end
