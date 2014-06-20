02bd4a340be8ae08e6f270bb52882bf360310035
Emma Follender <efollender@gmail.com>
2014-06-20 12:28:09 -0400

    Added getter methods

--- cookbook.rb
+++ cookbook.rb
@@ -2,6 +2,9 @@ class Cookbook
 	def initialize(title)
 		@title = title
 	end
+	def title
+		@title
+	end
 end
 
 class Recipe
@@ -10,4 +13,20 @@ class Recipe
 		@ingredients = ingredients
 		@steps = steps
 	end
+	def title
+		@title
+	end
+	def ingredients
+		@ingredients
+	end
+	def steps
+		@steps
+	end
 end
+
+mex_cuisine = Cookbook.new("Mexican Cooking")
+burrito = Recipe.new("Bean burrito",["Tortilla","bean"],["heat beans","place beans in tortilla", "roll up"])
+puts mex_cuisine.title # Mexican Cooking
+puts burrito.title # Bean Burrito
+p burrito.ingredients # ["tortilla", "bean", "cheese"]
+p burrito.steps # ["heat beans", "heat tortilla", "place beans in tortilla", "sprinkle cheese on top", "roll up"]
