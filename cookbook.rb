3b11ba8251f10f6de490df00ca924fffc93a0cfd
Emma Follender <efollender@gmail.com>
2014-06-20 12:33:52 -0400

    Added setter methods

--- cookbook.rb
+++ cookbook.rb
@@ -5,6 +5,9 @@ class Cookbook
 	def title
 		@title
 	end
+	def title=(new_title)
+		@title = new_title
+	end
 end
 
 class Recipe
@@ -16,11 +19,20 @@ class Recipe
 	def title
 		@title
 	end
+	def title=(new_title)
+		@title = new_title
+	end
 	def ingredients
 		@ingredients
 	end
+	def ingredients=(new_ingredients)
+		@ingredients = new_ingredients
+	end
 	def steps
 		@steps
+	end
+	def steps=(new_steps)
+		@steps = new_steps
 	end
 end
 
@@ -30,3 +42,12 @@ puts mex_cuisine.title # Mexican Cooking
 puts burrito.title # Bean Burrito
 p burrito.ingredients # ["tortilla", "bean", "cheese"]
 p burrito.steps # ["heat beans", "heat tortilla", "place beans in tortilla", "sprinkle cheese on top", "roll up"]
+mex_cuisine.title = "Mexican Recipes"
+puts mex_cuisine.title # Mexican Recipes
+burrito.title = "Veggie Burrito"
+burrito.ingredients = ["tortilla", "tomatoes"]
+burrito.steps = ["heat tomatoes", "place tomatoes in tortilla", "roll up"]
+
+p burrito.title # "Veggie Burrito"
+p burrito.ingredients # ["tortilla", "tomatoes"]
+p burrito.steps # ["heat tomatoes", "place tomatoes in tortilla", "roll up"]
