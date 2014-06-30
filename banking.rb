1addc925458689aa3cf84b8dd53eb98e32937ef4
Emma Follender <efollender@gmail.com>
2014-06-30 09:54:36 -0400

    Added transfer method to Bank class

--- banking.rb
+++ banking.rb
@@ -7,23 +7,31 @@ class Person
 	end
 end
 class Bank
+	attr_accessor :accounts
 	def initialize(name)
 		@name = name
-		@@accounts = {}
+		@accounts = {}
 	end	
 	def open_account(person)
-		@@accounts[person.name] = person.cash
+		@accounts[person.name] = person.cash
 	end
 	def withdraw(person,amount)
-		if (@@accounts[person.name] > amount)
-			@@accounts[person.name] -= amount
-			puts "The new balance for #{person.name} is #{@@accounts[person.name]}"
+		if (@accounts[person.name] > amount)
+			@accounts[person.name] -= amount
+			puts "The new balance for #{person.name} is #{@accounts[person.name]}"
 		else
 			puts "Insufficient funds."
 		end
 	end
 	def deposit(person,amount)
-		@@accounts[person.name] += amount
-		puts "The new balance for #{person.name} is #{@@accounts[person.name]}"
+		@accounts[person.name] += amount
+		puts "The new balance for #{person.name} is #{@accounts[person.name]}"
+	end
+	def transfer(person_from,bank_to,person_to,amount)
+		@accounts[person_from.name] -= amount
+		bank_to.accounts[person_to.name] += amount
+		puts "#{person_from.name}'s new balance is #{@accounts[person_from.name]}."
+		puts "#{person_to.name}'s new balance is #{bank_to.accounts[person_to.name]}."
 	end
 end
+
