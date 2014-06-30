ae7cda406d61bf03b8e387c327066d92c5dc30ce
Emma Follender <emmafollender@ool-4572903e.dyn.optonline.net>
2014-06-30 09:32:04 -0400

    Added methods to the Bank class: open_account, withdraw, deposit

--- banking.rb
+++ banking.rb
@@ -1,11 +1,29 @@
 class Person
+	attr_reader :name
+	attr_accessor :cash
 	def initialize(name,cash)
 		@name = name
+		@cash = cash
 	end
 end
 class Bank
-	def initialize(name,place)
+	def initialize(name)
 		@name = name
-		@place = place
+		@@accounts = {}
 	end	
+	def open_account(person)
+		@@accounts[person.name] = person.cash
+	end
+	def withdraw(person,amount)
+		if (@@accounts[person.name] > amount)
+			@@accounts[person.name] -= amount
+			puts "The new balance for #{person.name} is #{@@accounts[person.name]}"
+		else
+			puts "Insufficient funds."
+		end
+	end
+	def deposit(person,amount)
+		@@accounts[person.name] += amount
+		puts "The new balance for #{person.name} is #{@@accounts[person.name]}"
+	end
 end
