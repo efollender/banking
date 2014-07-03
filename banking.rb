967dbe270f3fe45db05ddc754ad43e8bd1e47387
Emma Follender <efollender@gmail.com>
2014-07-03 15:04:05 -0400

    Modified punch method so that it takes a person object and lowers their caffeine level

--- banking.rb
+++ banking.rb
@@ -15,8 +15,8 @@ class Bank
 		@@all_banks << name
 	end	
 	def open_account(person)
-		@accounts[person.name] = person.cash
-		puts "A new account has been created for #{person.name} with a balance of #{person.cash}"
+		@accounts[person.name] = 0
+		puts "A new account has been created for #{person.name}!"
 	end
 	def withdraw(person,amount)
 		if (@accounts[person.name] > amount)
@@ -28,18 +28,38 @@ class Bank
 	end
 	def deposit(person,amount)
 		@accounts[person.name] += amount
+		person.cash = (person.cash-amount)
 		puts "The new balance for #{person.name} is #{@accounts[person.name]}"
 	end
 	def transfer(person_from,bank_to,person_to,amount)
-		@accounts[person_from.name] -= amount
-		bank_to.accounts[person_to.name] += amount
-		puts "#{person_from.name}'s new balance is #{@accounts[person_from.name]}."
-		puts "#{person_to.name}'s new balance is #{bank_to.accounts[person_to.name]}."
+		if ( @accounts[person_from.name] > amount )
+			@accounts[person_from.name] -= amount
+			bank_to.accounts[person_to.name] += amount
+			puts "#{person_from.name}'s new balance is #{@accounts[person_from.name]}."
+			puts "#{person_to.name}'s new balance is #{bank_to.accounts[person_to.name]}."
+		else
+			puts "Insufficient funds."
+		end
 	end
 	def bank_balance
 		total = 0
 		@accounts.each {|k,v| total+= v}
-		puts "#{@name} Bank has a current balance os #{total}."
+		puts "#{@name} Bank has a current balance of #{total}."
 	end
 end
 
+emma = Person.new("Emma", 12500)
+brent = Person.new("Brent", 30700)
+chase = Bank.new("Chase")
+valley = Bank.new("Valley")
+chase.open_account(brent)
+valley.open_account(emma)
+valley.deposit(emma, 3000)
+chase.deposit(brent, 12000)
+chase.withdraw(brent,500)
+valley.withdraw(emma, 6000)
+chase.transfer(brent,valley,emma,2000)
+kait = Person.new("Kait", 7000)
+valley.open_account(kait)
+valley.deposit(kait,5500)
+valley.bank_balance
